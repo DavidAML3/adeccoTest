7a055c55b57416cb69bb7faaf04e4c20f9eecd68
DavidAML3 <David.aml.3@hotmail.com>
2021-09-01 19:01:51 -0500

    Created views and working app with moviesdb

---
+++ adeccoTest/Controller/MovieViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  adeccoTest
+//
+//  Created by David Andres Mejia Lopez on 30/08/21.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var viewModel = MovieViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadPopularMoviesData()
+    }
+    
+    private func loadPopularMoviesData() {
+        viewModel.fetchPopularMoviesData { [weak self] in
+            self?.tableView.dataSource = self
+            self?.tableView.reloadData()
+        }
+    }
+}
+
+// MARK: - TableView
+extension MovieViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection(section: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MovieTableViewCell
+        
+        let movie = viewModel.cellForRowAt(indexPath: indexPath)
+        cell.setCellWithValuesOf(movie)
+        return cell
+    }
+}

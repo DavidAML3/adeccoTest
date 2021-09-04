f0d8e2add2a613b0e21791398d6e38a288f0319f
DavidAML3 <David.aml.3@hotmail.com>
2021-09-03 22:20:11 -0500

    Changed logic for view from tableView to CollectionView

--- adeccoTest/Controller/MovieViewController.swift
+++ adeccoTest/Controller/MovieViewController.swift
@@ -88,7 +88,6 @@ extension MovieViewController: UICollectionViewDelegate {
             
             viewModel.getImageDataFromAsync(url: posterImageURL) { movieImage in
                 guard let movieImage = movieImage else {
-                    
                     return
                 }
                 vc.mImage = movieImage
@@ -98,8 +97,7 @@ extension MovieViewController: UICollectionViewDelegate {
                 vc.mOverview = "No overview registed for \(movie.title ?? "This movie")"
                 return
             }
-//            vc.mOverview = movie.overview!
-            vc.mOverview = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+            vc.mOverview = movie.overview!
         }
     }
 }
